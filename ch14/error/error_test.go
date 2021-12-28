5d6323e529962257ee30aa09f18c5344c611b737
wumuchun <403660598@qq.com>
2021-12-28 19:27:08 +0800

    及早失败，避免嵌套！

--- ch14/error/error_test.go
+++ ch14/error/error_test.go
@@ -8,6 +8,7 @@ package error
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -31,7 +32,44 @@ func GetFibonacci(n int) ([]int,error)  {
 	return fibList,nil
 }
 
+
+func GetFibonacci1(str string){
+	var (
+		i int
+		err error
+		list  []int
+	)
+
+	if i,err = strconv.Atoi(str);err == nil{
+		if list,err  = GetFibonacci(i);err == nil {
+			fmt.Println(list)
+		}else {
+			fmt.Println("Error",err)
+		}
+	}else {
+		fmt.Println("Error",err)
+	}
+}
+
 //及早失败，避免嵌套！
+func GetFibonacci2(str string){
+	var (
+		i int
+		err error
+		list  []int
+	)
+	if i,err = strconv.Atoi(str);err != nil{
+		fmt.Println("Error",err)
+		return
+	}
+	if list, err = GetFibonacci(i); err != nil {
+		fmt.Println("Error",err)
+		return
+	}
+	fmt.Println(list)
+}
+
+
 func TestGetFibonacci(t *testing.T)  {
 	if v,err := GetFibonacci(1);err !=nil {
 		if err== LessThanTwoError {
